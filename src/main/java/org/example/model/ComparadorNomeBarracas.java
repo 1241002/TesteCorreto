620b5bfdf8fc5a11c6f346050db369df26432297
1230604 <1230604@isep.ipp.pt>
2025-04-30 15:58:34 +0100

    2 commit com interface inicial

---
+++ src/main/java/org/example/model/ComparadorNomeBarracas.java
@@ -0,0 +1,4 @@
+package org.example.model;
+// -> Listar os nomes dos voluntários das barracas ordenados por nº de aluno
+public class ComparadorNomeBarracas {
+}
---
+++ src/main/java/org/example/model/ComparadorVendasBarracasCateg.java
@@ -0,0 +1,4 @@
+package org.example.model;
+// Comparador de vendas das barracas por categoria (bronze/prata/ouro) -> listar por ordem decrescente
+public class ComparadorVendasBarracasCateg {
+}
