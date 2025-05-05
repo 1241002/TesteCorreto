b11eca3634c7395511a69392ccb73cf8e023d174
1230604 <1230604@isep.ipp.pt>
2025-05-05 16:06:46 +0100

    comentarios nas interfaces

--- src/main/java/org/example/model/IStocksFinaisDiariosBarracas.java
+++ src/main/java/org/example/model/IStocksFinaisDiariosBarracas.java
@@ -1,4 +1,9 @@
 package org.example.model;
 
 public interface IStocksFinaisDiariosBarracas {
+    /* stock final diário da barraca:
+        > 100 -> Bronze;
+        50 a 100 -> Prata;
+        < 50 -> Ouro;
+     */
 }
--- src/main/java/org/example/model/IVendasVoluntarios.java
+++ src/main/java/org/example/model/IVendasVoluntarios.java
@@ -1,4 +1,9 @@
 package org.example.model;
 
 public interface IVendasVoluntarios {
+    /* Vendas dos voluntários:
+         < 500€ -> Bronze;
+         500€ a 1000€ -> Prata;
+         > 1000€ -> Ouro;
+     */
 }
