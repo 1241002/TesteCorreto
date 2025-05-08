16fc417a7f3163d5cd29a15ccc5a2ab57702d10b
1230604 <1230604@isep.ipp.pt>
2025-05-08 11:49:55 +0100

    commit inicial

--- src/main/java/org/example/model/Voluntario.java
+++ src/main/java/org/example/model/Voluntario.java
@@ -5,9 +5,38 @@ public class Voluntario {
     private int numeroAluno;
     private Federacao federacao;
 
+
     public Voluntario(String nome, int numeroAluno, Federacao federacao) {
         this.nome = nome;
         this.numeroAluno = numeroAluno;
+        this.federacao = federacao;
+    }
+
+    public Voluntario(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroAluno() {
+        return numeroAluno;
+    }
+
+    public void setNumeroAluno(int numeroAluno) {
+        this.numeroAluno = numeroAluno;
+    }
+
+    public Federacao getFederacao() {
+        return federacao;
+    }
+
+    public void setFederacao(Federacao federacao) {
         this.federacao = federacao;
     }
 }
