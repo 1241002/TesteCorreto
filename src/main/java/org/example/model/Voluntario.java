1d5dd7bcb269bbcd28582b583ccd5573da1bf46b
1230604 <1230604@isep.ipp.pt>
2025-05-08 11:19:16 +0100

    commit inicial

--- src/main/java/org/example/model/Voluntario.java
+++ src/main/java/org/example/model/Voluntario.java
@@ -1,4 +1,13 @@
 package org.example.model;
 
 public class Voluntario {
+    private String nome;
+    private int numeroAluno;
+    private Federacao federacao;
+
+    public Voluntario(String nome, int numeroAluno, Federacao federacao) {
+        this.nome = nome;
+        this.numeroAluno = numeroAluno;
+        this.federacao = federacao;
+    }
 }
